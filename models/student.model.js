7b8bcc7454ccd32efb476d9beef3d1fce41fce92
Oluwaseun Oloto <oloto.oluwaseun@gmail.com>
2025-05-19 20:35:41 -0700

    worked on schema

--- models/student.model.js
+++ models/student.model.js
@@ -1,6 +1,21 @@
 const students = [
-    {id:1, name: "Oluwaseun", email:"oluwaseun.oloto@gmail.com", password:"11111"},
-    {id:2, name: "Olayinka", email:"olayinka.oloto@gmail.com", password:"11111"},
-    {id:3, name: "Olatunji oluyide", email:"olatunji.oluyide@gmail.com", password:"11111"}
+    {
+        id:1, 
+        name: "Oluwaseun", 
+        email:"oluwaseun.oloto@gmail.com", 
+        password:"11111"
+    },
+
+    {id:2, 
+        name: "Olayinka", 
+        email:"olayinka.oloto@gmail.com", 
+        password:"11111"
+    },
+    
+    {id:3, 
+        name: "Olatunji oluyide", 
+        email:"olatunji.oluyide@gmail.com", 
+        password:"11111"
+    }
 ]
 module.exports =students
